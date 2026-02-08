cf4d2bdbdaa9e5fb90b8160d56990bba8c750588
vaibhav-velhal <vaibhav.velhal1614@gmail.com>
2026-02-08 21:48:19 +0530

    Project link added

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -18,10 +18,10 @@ function Home() {
                     <div className="project-info col-12 col-md-6">
                         <h3>JustPizza Project</h3>
                         <p>JustPizza is a responsive pizza ordering website built using React.js. It allows customers to view the menu, select their pizzas, and contact the restaurant for orders. The website is hosted on Vercel and deployed using GitHub Actions for continuous integration.</p>
-                        <p><b>Technologies: React.js, React Router, Bootstrap, CSS, Vite, Web3Forms, Vercel Environment Variables</b></p>
+                        <p className='technology-section fw-semibold'>Technologies: React.js, React Router, Bootstrap, CSS, Vite, Web3Forms, Vercel Environment Variables</p>
                         <div className="buttons-section">
                             <button className="btn btn-light btn-outline-dark py-2 px-3">View Details <FaArrowRight className='ms-1' /></button>
-                            <button className="btn btn-light btn-outline-dark py-2 px-3">View Preview <BsGlobe className='ms-1' /></button>
+                            <a className="btn btn-light btn-outline-dark py-2 px-3" href="https://just-pizza-official.vercel.app/" target='_blank'>View Preview <BsGlobe className='ms-1' /></a>
                         </div>
                     </div>
                     <div className="project-image col-12 col-md-6 border p-3 rounded-4">

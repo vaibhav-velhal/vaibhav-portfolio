c82f88210fcf94c7ef8b24651f6248866d1717e1
vaibhav-velhal <vaibhav.velhal1614@gmail.com>
2026-02-09 16:59:25 +0530

    Second project added

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -14,32 +14,44 @@ function Home() {
             </header>
 
             <section className="projects-section">
-                <h2 className='mb-4 fw-semibold'><FaDiamond className='mb-2' size={40} style={{color: 'hsl(206, 82%, 40%)'}} /> Projects</h2>
+                <h2 className='fw-semibold'><FaDiamond className='mb-2' style={{color: '#1271ba'}} /> Projects</h2>
                 
                 {/* First Project */}
-                <div className="project-item row mb-5">
-                    <div className="project-info col-12 col-md-6 pe-4">
-                        <h3>JustPizza Project</h3>
-                        <p>JustPizza is a responsive pizza ordering website built using React.js. It allows customers to view the menu, select their pizzas, and contact the restaurant for orders. The website is hosted on Vercel and deployed using GitHub Actions for continuous integration.</p>
-                        <p className='technology-section fw-semibold'>Technologies: React.js, React Router, Bootstrap, CSS, Vite, Web3Forms, Vercel Environment Variables</p>
-                        <div className="buttons-section">
-                            <button className="btn btn-light btn-outline-dark py-2 px-3">View Details <FaArrowRight className='ms-1' /></button>
-                            <a className="btn btn-light btn-outline-dark py-2 px-3" href="https://just-pizza-official.vercel.app/" target='_blank'>View Preview <BsGlobe className='ms-1' /></a>
+                <div className="project-item mb-4 mb-md-5">
+                    <h3 className='text-decoration-underline'>JustPizza</h3>
+                    <div className="row reverse">                        
+                        <div className="project-info col-12 col-md-6">
+                            <p className='project-description'>JustPizza is a responsive pizza ordering website built using React.js. It allows customers to view the menu, select their pizzas, and contact the restaurant for orders. The website is hosted on Vercel and deployed using GitHub Actions for continuous integration.</p>
+                            <p className='technology-section fw-semibold'>Technologies: React.js, React Router, Bootstrap, CSS, Vite, Web3Forms, Vercel Environment Variables</p>
+                            <div className="buttons-section">
+                                <button className="btn btn-light btn-outline-dark py-md-2 px-md-3">View Details <FaArrowRight className='ms-1' /></button>
+                                <a className="btn btn-light btn-outline-dark py-md-2 px-md-3" href="https://just-pizza-official.vercel.app/" target='_blank'>View Preview <BsGlobe className='ms-1' /></a>
+                            </div>
                         </div>
-                    </div>
-                    <div className="project-image col-12 col-md-6 border p-3 rounded-4">
-                        <img src="/JustPizza-Project.png" alt="JustPizza-Project" />
+                        <div className="image-container project-image col-12 col-md-6 ps-md-4">
+                            <img src="/JustPizza-Project.png" alt="JustPizza-Project" />
+                        </div>
                     </div>
                 </div>
 
+                <hr />
+
                 {/* Second Project */}
-                <div className="project-section row">
-                    <div className="project-image col-12 col-md-6 border p-3 rounded-4">
-                        <img src="" alt="" />
-                    </div>
-                    <div className="project-info col-12 col-md-6 pe-4">
-                        <h3>A Currency Converter</h3>
-                    </div>            
+                <div className="project-item my-4 my-md-5">
+                    <h3 className='text-decoration-underline'>Currency Converter</h3>
+                    <div className="row">
+                        <div className="image-container project-image col-12 col-md-6 pe-md-4">
+                            <img src="/" alt="Currency-Converter-Project" />
+                        </div>
+                        <div className="project-info col-12 col-md-6">
+                            <p className='project-description'>Currency Converter is a web app that provides real-time currency exchange rates. It uses the Frankfurter API for live conversion data and FlagsAPI for displaying country flags. The website supports multiple currencies and displays the most up-to-date exchange rates.</p>
+                            <p className='technology-section fw-semibold'>Technologies: HTML, CSS, JavaScript, Frankfurter API, FlagsAPI</p>
+                            <div className="buttons-section">
+                                <button className="btn btn-light btn-outline-dark py-2 px-3">View Details <FaArrowRight className='ms-1' /></button>
+                                <a className="btn btn-light btn-outline-dark py-2 px-3" href="https://vaibhav-velhal.github.io/currency-converter/" target='_blank'>View Preview <BsGlobe className='ms-1' /></a>
+                            </div>
+                        </div>
+                    </div>                    
                 </div>
             </section>
         </section>

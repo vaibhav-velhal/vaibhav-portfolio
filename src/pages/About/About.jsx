90fe48c9946fff5822d8ebd49ed0edb2931414c2
vaibhav-velhal <vaibhav.velhal1614@gmail.com>
2026-02-25 21:39:05 +0530

    Header and about section added at About page

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,10 +1,17 @@
 import './About.css';
 
 function About() {
-    return(
-        <div>
-            
-        </div>
+    return (
+        <section className='about-page text-center'>
+            <header className='header-section mb-md-5'>
+                <h3>I'm a Frontend Developer</h3>
+            </header>
+            <section className='about-section pt-md-3'>
+                <h3 className='mb-md-3'>About Me</h3>
+                <p>I'm Vaibhav Velhal, a passionate Frontend Developer from India. I specialize in building responsive and user-friendly web applications using modern technologies like HTML, CSS, Bootstrap, JavaScript, and React.js.
+                    I enjoy creating clean UI designs and writing efficient code to deliver smooth user experiences.</p>
+            </section>
+        </section>
     )
 }
 

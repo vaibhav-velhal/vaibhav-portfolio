386308706ca6e7bc4a7b3ca318761a8bce84ddbe
vaibhav-velhal <vaibhav.velhal1614@gmail.com>
2026-02-28 19:25:27 +0530

    Profile picture section added and about section updated

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -2,14 +2,22 @@ import './About.css';
 
 function About() {
     return (
-        <section className='about-page text-center'>
-            <header className='header-section mb-md-5'>
-                <h3>I'm a Frontend Developer</h3>
+        <section className='about-page'>
+            <header className='header-section mb-md-5 reverse'>
+                <p className='text-center'>I'm a Frontend Developer <br /> specializing in <strong>React.js</strong>.</p>
+                <div className="profile-picture-section">
+                    <div className="image-container">
+                        <img src="/Profile Picture.jpg" alt="Vaibhav-Velhal-Profile-Picture" loading='lazy' />
+                    </div>
+                    <p className='text-center m-0 mt-2 mb-2'><small>Er. Vaibhav Velhal</small></p>
+                </div>
             </header>
-            <section className='about-section pt-md-3'>
-                <h3 className='mb-md-3'>About Me</h3>
-                <p>I'm Vaibhav Velhal, a passionate Frontend Developer from India. I specialize in building responsive and user-friendly web applications using modern technologies like HTML, CSS, Bootstrap, JavaScript, and React.js.
-                    I enjoy creating clean UI designs and writing efficient code to deliver smooth user experiences.</p>
+
+            <section className='about-section mb-5'>
+                <h3 className='mb-4'>About Me</h3>
+                <p>I'm <b>Vaibhav Velhal</b>, a passionate <b>Frontend Developer</b> from India. Completed my B.Tech in <b>Electronics and Telecommunication Engineering.</b></p>
+                <p>I specialize in building responsive and user-friendly web applications using modern technologies like <b>HTML, CSS, Bootstrap, JavaScript,</b> and <b>React.js</b>.
+                    I enjoy creating clean <b>UI designs</b> and writing efficient code to deliver smooth <b>user experiences</b>.</p>
             </section>
         </section>
     )

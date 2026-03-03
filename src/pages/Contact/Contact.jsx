cd9e97e9b1d5a972d095dbf1ac2a8a482cc239fd
vaibhav-velhal <vaibhav.velhal1614@gmail.com>
2026-03-03 17:25:57 +0530

    Hero section added at Contact page

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,10 +1,26 @@
 import './Contact.css';
 
-function Contact(){
+function Contact() {
 
-    return(
+    return (
         <section className="contact-page">
-            
+
+            <div className="contact-hero text-center">
+                <h1 className="contact-title mb-3">Let's work <strong>together</strong></h1>
+                <p className="contact-subtext text-secondary">
+                    I'm always open to discussing new projects, creative ideas, <br />
+                    or opportunities to be part of your vision. <br />
+                    Whether you need <span className='text-dark fw-semibold'>a responsive website</span> or <span className='text-dark fw-semibold'>a modern React application</span>, <br />
+                    I'd love to connect.
+                </p>
+            </div>
+
+            <div className="contact-cards">
+                <div className="card text-center p-3 rounded-5">
+                    <h3>Get in touch</h3>
+                </div>
+            </div>
+
         </section>
     )
 }

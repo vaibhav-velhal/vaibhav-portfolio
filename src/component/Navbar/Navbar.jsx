697eaccd361454a5c2030c8fd8376d18f5fa8a0f
vaibhav-velhal <vaibhav.velhal1614@gmail.com>
2026-03-17 20:07:56 +0530

    Navbar toggler icon updated into hamburger

--- src/component/Navbar/Navbar.jsx
+++ src/component/Navbar/Navbar.jsx
@@ -1,23 +1,27 @@
-import {NavLink} from 'react-router-dom';
+import { useState } from 'react';
+import { NavLink } from 'react-router-dom';
 import './Navbar.css';
 
 function Navbar() {
 
-    return(
-        <nav className="navbar navbar-expand-lg fixed-top"
-        aria-label="Primary Navigation">
-            <a href="/" className="navbar-brand fw-semibold text-dark">Vaibhav Velhal</a>
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-            </button>
+    const [isOpen, setIsOpen] = useState(false);
 
-            <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
-                    <ul className="navbar-nav gap-1 gap-md-4 m-2 m-lg-0">
-                        <li className="nav-item m-1 m-lg-0"><NavLink to="/">Home</NavLink></li>
-                        <li className="nav-item m-1 m-lg-0"><NavLink to="/About">About</NavLink></li>
-                        <li className="nav-item m-1 m-lg-0"><NavLink to="/Contact">Contact</NavLink></li>
-                    </ul>
+    return (
+        <nav className="navbar navbar-expand-lg fixed-top"
+            aria-label="Primary Navigation">
+            <a href="/" className="navbar-brand fw-semibold text-dark">Vaibhav Velhal</a>
+
+            <div className={`hamburger ${isOpen ? "active" : ""}`} onClick={() => setIsOpen(!isOpen)}>
+                <span></span>
+                <span></span>
+            </div>
+
+            <div className={`collapse navbar-collapse justify-content-end ${isOpen ? "show" : ""}`} id="navbarNav">
+                <ul className="navbar-nav gap-1 gap-md-4 m-2 m-lg-0">
+                    <li className="nav-item m-1 m-lg-0"><NavLink to="/">Home</NavLink></li>
+                    <li className="nav-item m-1 m-lg-0"><NavLink to="/About">About</NavLink></li>
+                    <li className="nav-item m-1 m-lg-0"><NavLink to="/Contact">Contact</NavLink></li>
+                </ul>
             </div>
         </nav>
     )
